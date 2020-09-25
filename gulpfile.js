cbfadf2d2dda0054717655d3dcb33c6a96f798d6
anya <schepelina.an@yandex.ru>
2020-09-25 16:10:44 +0400

    changed images paths

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const sass = require('gulp-sass')
 const babel = require('gulp-babel')
 const imagemin = require('gulp-imagemin')
 const uglify = require('gulp-uglify')
+const svgToCss = require('gulp-svg-to-css');
 
 gulp.task('styles', function () {
     gulp.src('resources/scss/style.scss')
@@ -19,9 +20,23 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('public/js/'))
 });
 
+gulp.task('images', function() {
+  gulp.src('resources/img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('public/images/'))
+});
+//
+// gulp.task('svg', function() {
+//     gulp.src('resources/icon/**/*.svg')
+//     .pipe(svgToCss('svg.css'))
+//     .pipe(gulp.dest('public/icon/'));
+// });
+
 gulp.task('watch', function () {
     gulp.watch('resources/scss/**/*.scss', gulp.parallel('styles'));
     gulp.watch('resources/js/*.js', gulp.parallel('scripts'));
+    gulp.watch('resources/images/**/*', gulp.parallel('images'));
+    // gulp.watch('resources/icon/**/*.svg', gulp.parallel('svg'));
 });
 
-gulp.task('default', gulp.parallel('styles', 'scripts', 'watch'));
+gulp.task('default', gulp.parallel('styles', 'scripts', 'images', 'watch'));
